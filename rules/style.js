cfdbd912617d579b73e88ae6bdb938238db51f11
sibiraj-s <dev.sibiraj@outlook.com>
2020-07-20 11:40:34 +0530

    Sort max-len rule keys

--- rules/style.js
+++ rules/style.js
@@ -7,11 +7,11 @@ module.exports = {
     // specify the maximum length of a line in your program
     // https://eslint.org/docs/rules/max-len
     'max-len': ['error', 120, 2, {
-      ignoreUrls: true,
       ignoreComments: false,
       ignoreRegExpLiterals: true,
       ignoreStrings: true,
       ignoreTemplateLiterals: true,
+      ignoreUrls: true,
     }],
 
     // disallow multiple empty lines, only one newline at the end, and no new lines at the beginning

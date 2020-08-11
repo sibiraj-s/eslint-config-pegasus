9bdc30d4451fa2331cbafe9dec1836a886f9115f
sibiraj-s <dev.sibiraj@outlook.com>
2020-08-11 18:08:33 +0530

    Add rule for-direction

---
+++ rules/errors.js
@@ -0,0 +1,7 @@
+module.exports = {
+  rules: {
+    // enforce "for" loop update clause moving the counter in the right direction
+    // https://eslint.org/docs/rules/for-direction
+    'for-direction': 'error',
+  },
+};

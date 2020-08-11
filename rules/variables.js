fb5475eb3b6f8d2d3f9aa6e8b7181272d78b2fab
sibiraj-s <dev.sibiraj@outlook.com>
2020-08-11 21:30:51 +0530

    Add rule no-label-var

--- rules/variables.js
+++ rules/variables.js
@@ -7,5 +7,9 @@ module.exports = {
     // disallow deleting variables
     // https://eslint.org/docs/rules/no-delete-var
     'no-delete-var': 'error',
+
+    // disallow Labels That Are Variables Names
+    // https://eslint.org/docs/rules/no-label-var
+    'no-label-var': 'error',
   },
 };

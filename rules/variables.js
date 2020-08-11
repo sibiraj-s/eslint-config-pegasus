281f8993c640c7e895cfc22e3b9986bc5d45d794
sibiraj-s <dev.sibiraj@outlook.com>
2020-08-11 22:11:47 +0530

    Update rule init-declarations

--- rules/variables.js
+++ rules/variables.js
@@ -2,7 +2,7 @@ module.exports = {
   rules: {
     // require or disallow initialization in variable declarations
     // https://eslint.org/docs/rules/init-declarations
-    'init-declarations': ['error', 'never'],
+    'init-declarations': 'off',
 
     // disallow deleting variables
     // https://eslint.org/docs/rules/no-delete-var

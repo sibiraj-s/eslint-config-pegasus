74a8decd908d7aa87d13332c58ffcca28543c0e4
sibiraj-s <dev.sibiraj@outlook.com>
2020-08-11 18:12:54 +0530

    Add rule getter-return

--- rules/errors.js
+++ rules/errors.js
@@ -3,5 +3,9 @@ module.exports = {
     // enforce "for" loop update clause moving the counter in the right direction
     // https://eslint.org/docs/rules/for-direction
     'for-direction': 'error',
+
+    // enforces that a return statement is present in property getters
+    // https://eslint.org/docs/rules/getter-return
+    'getter-return': ['error', { allowImplicit: true }],
   },
 };

44682561b5e6851f228a8765c6805dc9652e47fd
sibiraj-s <dev.sibiraj@outlook.com>
2020-08-11 21:29:59 +0530

    Add rule no-delete-var

--- rules/variables.js
+++ rules/variables.js
@@ -3,5 +3,9 @@ module.exports = {
     // require or disallow initialization in variable declarations
     // https://eslint.org/docs/rules/init-declarations
     'init-declarations': ['error', 'never'],
+
+    // disallow deleting variables
+    // https://eslint.org/docs/rules/no-delete-var
+    'no-delete-var': 'error',
   },
 };

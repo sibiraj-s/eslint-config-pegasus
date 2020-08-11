9703389e7dcee3f184a8a8d23cb86ccb4d16ba58
sibiraj-s <dev.sibiraj@outlook.com>
2020-08-11 22:30:23 +0530

    Add rule no-undef

--- rules/errors.js
+++ rules/errors.js
@@ -179,5 +179,9 @@ module.exports = {
     // disallow Shadowing of Restricted Names
     // https://eslint.org/docs/rules/no-shadow-restricted-names
     'no-shadow-restricted-names': 'error',
+
+    // disallow Undeclared Variables
+    // https://eslint.org/docs/rules/no-undef
+    'no-undef': 'error',
   },
 };

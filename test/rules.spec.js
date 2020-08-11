bf4f89130f1fee354cfd62b572502360c60c62e2
sibiraj-s <dev.sibiraj@outlook.com>
2020-08-11 22:43:40 +0530

    Update tests

--- test/rules.spec.js
+++ test/rules.spec.js
@@ -8,7 +8,7 @@ const eslint = new ESLint({
 });
 
 it('should validate the code correctly', async () => {
-  const code = 'const foo = {\n  x: 1,\n  y: 2,\n};\n';
+  const code = 'const foo = {\n  x: 1,\n  y: 2,\n};\nfoo.x = 3;\n';
   const errors = await eslint.lintText(code);
 
   expect(errors[0].errorCount).toBe(0);

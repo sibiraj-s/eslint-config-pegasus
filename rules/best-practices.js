716280e1823c8e232da429dd168afa3c14a88c59
sibiraj-s <dev.sibiraj@outlook.com>
2020-08-12 09:15:31 +0530

    Add rule array-callback-return

--- rules/best-practices.js
+++ rules/best-practices.js
@@ -3,5 +3,9 @@ module.exports = {
     // enforces getter/setter pairs in objects and classes
     // https://eslint.org/docs/rules/accessor-pairs
     'accessor-pairs': 'error',
+
+    // enforces return statements in callbacks of array's methods
+    // https://eslint.org/docs/rules/array-callback-return
+    'array-callback-return': ['error', { allowImplicit: true }],
   },
 };

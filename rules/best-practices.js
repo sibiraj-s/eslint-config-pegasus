8cd0718b823c9f6d5191b1cb8fd97147ab340b6a
sibiraj-s <dev.sibiraj@outlook.com>
2020-08-12 09:14:23 +0530

    Add rule accessor-pairs

---
+++ rules/best-practices.js
@@ -0,0 +1,7 @@
+module.exports = {
+  rules: {
+    // enforces getter/setter pairs in objects and classes
+    // https://eslint.org/docs/rules/accessor-pairs
+    'accessor-pairs': 'error',
+  },
+};

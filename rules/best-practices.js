7b0bf133d10f9cba609f3f13581ba3875c1f60ec
sibiraj-s <dev.sibiraj@outlook.com>
2020-08-12 09:30:44 +0530

    Add rule complexity

--- rules/best-practices.js
+++ rules/best-practices.js
@@ -15,5 +15,9 @@ module.exports = {
     // enforce that class methods utilize this
     // https://eslint.org/docs/rules/class-methods-use-this
     'class-methods-use-this': 'error',
+
+    // limit cyclomatic complexity
+    // https://eslint.org/docs/rules/complexity
+    'complexity': ['error', 10],
   },
 };

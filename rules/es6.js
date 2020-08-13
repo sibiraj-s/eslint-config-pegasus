5b9e29f7277f160d1ce2feb5aff668b782a467c9
sibiraj-s <dev.sibiraj@outlook.com>
2020-08-13 22:06:08 +0530

    Add rule arrow-body-style

--- rules/es6.js
+++ rules/es6.js
@@ -7,6 +7,10 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    // require braces around arrow function bodies
+    // https://eslint.org/docs/rules/arrow-body-style
+    'arrow-body-style': ['error', 'as-needed'],
+
     // enforce the spacing around the * in generator functions
     // https://eslint.org/docs/rules/generator-star-spacing
     'generator-star-spacing': ['error', { before: false, after: true }],

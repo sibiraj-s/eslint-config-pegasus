a763dda03b1a62c99437360601d9ded9f74e34fe
sibiraj-s <dev.sibiraj@outlook.com>
2020-08-13 22:07:41 +0530

    Add rule arrow-parens

--- rules/es6.js
+++ rules/es6.js
@@ -11,6 +11,10 @@ module.exports = {
     // https://eslint.org/docs/rules/arrow-body-style
     'arrow-body-style': ['error', 'as-needed'],
 
+    // require parentheses around arrow function arguments
+    // https://eslint.org/docs/rules/arrow-parens
+    'arrow-parens': ['error', 'always'],
+
     // enforce the spacing around the * in generator functions
     // https://eslint.org/docs/rules/generator-star-spacing
     'generator-star-spacing': ['error', { before: false, after: true }],

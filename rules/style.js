cfbc0f2ca23b62053eebde9f7bf217e2eec0c976
sibiraj-s <dev.sibiraj@outlook.com>
2020-08-16 15:29:17 +0530

    Add rule no-nested-ternary

--- rules/style.js
+++ rules/style.js
@@ -259,6 +259,10 @@ module.exports = {
     // https://eslint.org/docs/rules/no-negated-condition
     'no-negated-condition': 'error',
 
+    // disallow nested ternary expressions
+    // https://eslint.org/docs/rules/no-nested-ternary
+    'no-nested-ternary': 'error',
+
     // disallow duplicate keys in object literals
     // https://eslint.org/docs/rules/no-dupe-keys
     'no-dupe-keys': 'error',

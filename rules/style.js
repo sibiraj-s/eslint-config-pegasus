de5926b1f588667afc93b2671f9927663efe499c
sibiraj-s <dev.sibiraj@outlook.com>
2020-08-16 15:25:52 +0530

    Add rule no-multi-assign

--- rules/style.js
+++ rules/style.js
@@ -247,6 +247,10 @@ module.exports = {
     // https://eslint.org/docs/rules/no-mixed-spaces-and-tabs
     'no-mixed-spaces-and-tabs': 'error',
 
+    // disallow use of chained assignment expressions
+    // https://eslint.org/docs/rules/no-multi-assign
+    'no-multi-assign': 'error',
+
     // disallow multiple empty lines, only one newline at the end, and no new lines at the beginning
     // https://eslint.org/docs/rules/no-multiple-empty-lines
     'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 0, maxEOF: 0 }],

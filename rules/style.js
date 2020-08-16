075d9c1c753d597abc6b6e6f52f923359c81296c
sibiraj-s <dev.sibiraj@outlook.com>
2020-08-16 16:41:02 +0530

    Add rule semi-style

--- rules/style.js
+++ rules/style.js
@@ -383,6 +383,10 @@ module.exports = {
     // https://eslint.org/docs/rules/semi-spacing
     'semi-spacing': ['error', { before: false, after: true }],
 
+    // enforce location of semicolons
+    // https://eslint.org/docs/rules/semi-style
+    'semi-style': ['error', 'last'],
+
     // sequire Or Disallow Space Before Blocks
     // https://eslint.org/docs/rules/space-before-blocks
     'space-before-blocks': 'error',

c0e3b297452472593df2200e7302d59321cacb4b
sibiraj-s <dev.sibiraj@outlook.com>
2020-08-18 08:43:44 +0530

    Remove duplicate rules

--- rules/errors.js
+++ rules/errors.js
@@ -173,17 +173,5 @@ module.exports = {
     // enforce comparing typeof expressions against valid strings
     // https://eslint.org/docs/rules/valid-typeof
     'valid-typeof': 'error',
-
-    // disallow variable declarations from shadowing variables declared in the outer scope
-    // https://eslint.org/docs/rules/no-shadow
-    'no-shadow': 'error',
-
-    // disallow Shadowing of Restricted Names
-    // https://eslint.org/docs/rules/no-shadow-restricted-names
-    'no-shadow-restricted-names': 'error',
-
-    // disallow Undeclared Variables
-    // https://eslint.org/docs/rules/no-undef
-    'no-undef': 'error',
   },
 };
